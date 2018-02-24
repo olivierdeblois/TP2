a5c2877cbca9217413d702133215e739131b2abd
olivierdeblois <olivier.deblois@outlook.com>
2018-02-23 21:29:16 -0500

    premier commit

---
+++ TP2/Client.cpp
@@ -0,0 +1,18 @@
+/*
+ * Client.cpp
+ *
+ *  Created on: 2018-02-23
+ *      Author: etudiant
+ */
+
+#include "Client.h"
+
+Client::Client() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Client::~Client() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ TP2/Client.h
@@ -0,0 +1,17 @@
+/*
+ * Client.h
+ *
+ *  Created on: 2018-02-23
+ *      Author: etudiant
+ */
+
+#ifndef CLIENT_H_
+#define CLIENT_H_
+
+class Client {
+public:
+	Client();
+	virtual ~Client();
+};
+
+#endif /* CLIENT_H_ */
